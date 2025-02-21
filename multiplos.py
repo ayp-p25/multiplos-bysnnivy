01b49cbcc03ff16a4db0dd6107548530ef23b983
bysnnivy <efrain.gonzalez@iteso.mx>
2025-02-21 08:11:54 -0600

    multiplos 1er intento

--- multiplos.py
+++ multiplos.py
@@ -2,14 +2,17 @@
 Inserta el encabezado aquí y escribe tu código abajo
 """
 
-# Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
+número1 = int(input("Ingrese un número: "))
+número2 = int(input("Ingrese otro número: "))
 
 # Proceso
+if número2 == 0:
+    resultado = "No se puede dividir por cero."
+elif número1 % número2 == 0:  
+    resultado = "El " + str(número1) + " es múltiplo de " + str(número2)
+else:  
+    resultado = "El " + str(número1) + " no es múltiplo de " + str(número2)
 
-
-# Salidas
-print(salida)
+# Salida
+print(resultado)
